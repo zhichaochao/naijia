bea50178dd68337ed0b2611a346a5f403c2015dc
zhichaochao <928583371@qq.com>
2018-09-07 16:11:49 +0800

    重新配置，忽然不需要重复的

--- admin/config.php
+++ admin/config.php
@@ -28,6 +28,6 @@ define('DB_DRIVER', 'mpdo');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'lx');
+define('DB_DATABASE', 'naijia');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
