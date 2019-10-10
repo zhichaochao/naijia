f791365b2447f887d400271df4cb669de544c796
lx0128 <1358432408@qq.com>
2019-10-10 15:05:41 +0800

    移动端name长度修改

--- mobilecatalog/controller/product/category.php
+++ mobilecatalog/controller/product/category.php
@@ -1377,7 +1377,7 @@ class ControllerProductCategory extends Controller {
 					'reviews'	  => $result['reviews'],
 					'percent'    => 100-$percents,
 					'hotsort'	  => $result['hot'],
-					'name'        => $result['name'],
+					'name'        => utf8_substr(strip_tags($result['name']),0,25).'...',
 					'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..',
 					'price'       => $this->currency->format($result['price'],$this->session->data['currency']),
 					'specials'	  => $specials,
