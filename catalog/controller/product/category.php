ca9c21fad49a3318bee3b1c2e7f12b08b4922e06
lx0128 <1358432408@qq.com>
2019-10-10 15:00:58 +0800

    截取name长度

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -1369,7 +1369,7 @@ class ControllerProductCategory extends Controller {
 					'max_name'	  => $result['name'],
 					'reviews'	  => $result['reviews'],
 					'percent'    => $percents,
-					'name'        => $result['name'],
+					'name'        => utf8_substr(strip_tags($result['name']),0,25).'...',
 					'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..',
 					'price'       => $this->currency->format($result['price'],$this->session->data['currency']),
 					'specials'	  => $specials,
